8680e6e7f615d3a26770fe71de1de09c397664d4
Maarten Bosma <mbosma@bkwi.nl>
2017-04-07 09:24:12 +0200

    US-004: exit - merge

--- src/main/java/command/library/CommandFactory.java
+++ src/main/java/command/library/CommandFactory.java
@@ -2,7 +2,7 @@
  * DOSBox, Scrum.org, Professional Scrum Developer Training
  * Authors: Rainer Grau, Daniel Tobler, Zuehlke Technology Group
  * Copyright (c) 2013 All Right Reserved
- */ 
+ */
 
 package command.library;
 
@@ -11,33 +11,34 @@ import java.util.ArrayList;
 import command.framework.Command;
 import interfaces.IDrive;
 
-/**The factory is responsible to create an object of every command supported
+/**
+ * The factory is responsible to create an object of every command supported
  * and to add it to the list of known commands.
  * New commands must be added to the list of known commands here.
  */
 public class CommandFactory {
-	
-	private ArrayList<Command> commands;
 
-	public CommandFactory(IDrive drive) {
-		this.commands = new ArrayList<Command>();
-		
-		this.commands.add(new CmdDir("dir", drive));
-		this.commands.add(new CmdCd("cd", drive));
-		this.commands.add(new CmdCd("chdir", drive));
-		this.commands.add(new CmdMkDir("mkdir", drive));
-		this.commands.add(new CmdMkDir("md", drive));
-		this.commands.add(new CmdMkFile("mf", drive));
-		this.commands.add(new CmdMkFile("mkfile", drive));
-		this.commands.add(new CmdVer("ver", drive));
-		this.commands.add(new CmdPrintFile("type",drive));
+    private ArrayList<Command> commands;
 
-		// Add your commands here
-		
-	}
-	
-	public ArrayList<Command> getCommandList() {
-		return commands;
-	}
+    public CommandFactory(IDrive drive) {
+        this.commands = new ArrayList<Command>();
+
+        this.commands.add(new CmdDir("dir", drive));
+        this.commands.add(new CmdCd("cd", drive));
+        this.commands.add(new CmdCd("chdir", drive));
+        this.commands.add(new CmdMkDir("mkdir", drive));
+        this.commands.add(new CmdMkDir("md", drive));
+        this.commands.add(new CmdMkFile("mf", drive));
+        this.commands.add(new CmdMkFile("mkfile", drive));
+        this.commands.add(new CmdVer("ver", drive));
+        this.commands.add(new CmdPrintFile("type", drive));
+        this.commands.add((new CmdExit("exit", drive)));
+        // Add your commands here
+
+    }
+
+    public ArrayList<Command> getCommandList() {
+        return commands;
+    }
 
 }
